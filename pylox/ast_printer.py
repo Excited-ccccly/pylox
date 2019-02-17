e6253d4047fba7f951cebadb3eea98df197a59fa
ccccly <geekchenlingyun@outlook.com>
2019-02-17 23:29:39 +0800

    AstPrinter Support statements

--- pylox/ast_printer.py
+++ pylox/ast_printer.py
@@ -1,8 +1,9 @@
 from pylox.expr import Expr
 from pylox.token import Token
 from pylox.expr import ExprVisitor
+from pylox.stmt import StmtVisitor
 
-class AstPrinter(ExprVisitor):
+class AstPrinter(ExprVisitor, StmtVisitor):
   def print(self, expr: Expr):
     return expr.accept(self)
 
@@ -59,3 +60,46 @@ class AstPrinter(ExprVisitor):
 
   def visit_variable_expr(self, expr):
     return expr.name.lexeme
+
+  def visit_block_stmt(self, stmt):
+    stmts = "".join([stmt.accept(self) for stmt in stmt.statements])
+    return f"(block {stmts})"
+
+  def visit_class_stmt(self, stmt):
+    name = f"(class {stmt.name.lexeme}"
+    if stmt.superclass:
+      name += f" < {stmt.superclass.accept(self)}"
+    methods = " ".join([method.accept(self) for method in stmt.methods])
+    return f"{name} {methods})"
+
+  def visit_expression_stmt(self, stmt):
+    return self.parenthesize(";", stmt.expression)
+
+  def visit_function_stmt(self, stmt):
+    name = f"(func {stmt.name.lexeme}("
+    tokens = " ".join([param.name.lexeme for param in stmt.params])
+    body = "".join([s.accept(self) for s in stmt.body.statements])
+    return f"{name}{tokens}){body})"
+
+  def visit_if_stmt(self, stmt):
+    if stmt.elseBranch:
+      return self.parenthesize2("if-else", stmt.condition, stmt.thenBranch, stmt.elseBranch)
+    return self.parenthesize("if", stmt.condition, stmt.thenBranch)
+
+  def visit_print_stmt(self, stmt):
+    return self.parenthesize("print", stmt.expression)
+
+  def visit_return_stmt(self, stmt):
+    if stmt.value:
+      return self.parenthesize("return", stmt.value)
+    return "(return)"
+
+  def visit_var_stmt(self, stmt):
+    if stmt.initializer:
+      return self.parenthesize2("var", stmt.name, "=", stmt.initializer)
+    return self.parenthesize2("var", stmt.name)
+
+  def visit_while_stmt(self, stmt):
+    return self.parenthesize2("while", stmt.condition, stmt.body)
+
+
