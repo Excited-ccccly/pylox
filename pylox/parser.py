2f4c908403eb95924d2d5669dad7a0d9a1f44606
ccccly <geekchenlingyun@outlook.com>
2019-02-19 22:19:44 +0800

    CFG documentation for lox parser.

--- pylox/parser.py
+++ pylox/parser.py
@@ -5,6 +5,8 @@ from pylox.stmt import Print, Expression, Var, Block, If, While, Function, Retur
 from pylox.error import ParseError, error_handler
 
 class Parser:
+  """A recursive descent parser for lox lang
+  """
 
   def __init__(self, tokens: List[Token]):
     self.current = 0
@@ -12,12 +14,21 @@ class Parser:
     self.token_count = len(tokens)
 
   def parse(self):
+    """
+    program        → declaration* EOF ;
+    """
     stmts = []
     while not self.__is_at_end():
       stmts.append(self.__declaration())
     return stmts
 
   def __declaration(self):
+    """
+    declaration    → classDecl
+                  | funDecl
+                  | varDecl
+                  | statement ;
+    """
     try:
       if self.__match(TokenType.VAR): return self.__var_declaration()
       if self.__match(TokenType.FUN): return self.__fun_declaration()
@@ -27,6 +38,10 @@ class Parser:
       self.__synchronize()
 
   def __class_declaration(self):
+    """
+    classDecl      → "class" IDENTIFIER ( "<" IDENTIFIER )?
+                 "{" function* "}" ;
+    """
     self.__advance()
     name: Token = self.__consume(TokenType.IDENTIFIER, "Expect class name.")
     superclass = None
@@ -40,10 +55,18 @@ class Parser:
     return Class(name, superclass=superclass, methods=methods)
 
   def __fun_declaration(self):
+    """
+    funDecl        → "fun" function ;
+    """
     self.__advance()
     return self.__function("function")
 
   def __function(self, kind: str):
+    """
+    function       → IDENTIFIER "(" parameters? ")" block ;
+    parameters     → IDENTIFIER ( "," IDENTIFIER )* ;
+    arguments      → expression ( "," expression )* ;
+    """
     name: Token = self.__consume(TokenType.IDENTIFIER, err_msg=f"Expect {kind} name")
     self.__consume(TokenType.LEFT_PAREN, f"Expect '(' after {kind} name")
     params = []
@@ -58,6 +81,9 @@ class Parser:
     return Function(name, params, body)
 
   def __var_declaration(self):
+    """
+    varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
+    """
     self.__advance()
     name: Token = self.__consume(TokenType.IDENTIFIER, err_msg="Expect variable name")
     initializer: Expr = None
@@ -67,6 +93,15 @@ class Parser:
     return Var(name=name, initializer=initializer)
 
   def __stmt(self):
+    """
+    statement      → exprStmt
+                  | forStmt
+                  | ifStmt
+                  | printStmt
+                  | returnStmt
+                  | whileStmt
+                  | block ;
+    """
     if self.__match(TokenType.LEFT_BRACE): return self.__block()
     if self.__match(TokenType.FOR): return self.__for_stmt()
     if self.__match(TokenType.IF): return self.__if_stmt()
@@ -76,6 +111,9 @@ class Parser:
     return self.__expr_stmt()
 
   def __block(self):
+    """
+    block          → "{" declaration* "}" ;
+    """
     self.__advance()
     stmts = []
     while not self.__match(TokenType.RIGHT_BRACE) and not self.__is_at_end():
@@ -84,11 +122,20 @@ class Parser:
     return Block(statements=stmts)
 
   def __expr_stmt(self):
+    """
+    exprStmt       → expression ";" ;
+    """
     expr = self.__expression()
     self.__consume(expected=TokenType.SEMICOLON, err_msg="Expect ';' after statement.")
     return Expression(expr)
 
   def __for_stmt(self):
+    """ desugar to while statement.
+
+    forStmt        → "for" "(" ( varDecl | exprStmt | ";" )
+                           expression? ";"
+                           expression? ")" statement ;
+    """
     self.__advance()
     self.__consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.")
     initializer = None
@@ -116,6 +163,9 @@ class Parser:
     return body
 
   def __if_stmt(self):
+    """
+    ifStmt         → "if" "(" expression ")" statement ( "else" statement )? ;
+    """
     self.__advance()
     self.__consume(TokenType.LEFT_PAREN, "Expect '(' after 'if'.")
     condition = self.__expression()
@@ -127,12 +177,18 @@ class Parser:
     return If(condition, then_branch, else_branch)
 
   def __print_stmt(self):
+    """
+    printStmt      → "print" expression ";" ;
+    """
     self.__advance()
     expr = self.__expression()
     self.__consume(expected=TokenType.SEMICOLON, err_msg="Expect ';' after statement.")
     return Print(expr)
 
   def __return_stmt(self):
+    """
+    returnStmt     → "return" expression? ";" ;
+    """
     keyword = self.__peek()
     self.__advance()
     return_value = None
@@ -144,6 +200,9 @@ class Parser:
     return Return(keyword, return_value)
 
   def __while_stmt(self):
+    """
+    whileStmt      → "while" "(" expression ")" statement ;
+    """
     self.__advance()
     self.__consume(TokenType.LEFT_PAREN, "Expect '(' after 'if'.")
     condition = self.__expression()
@@ -152,9 +211,16 @@ class Parser:
     return While(condition, body)
 
   def __expression(self) -> Expr:
+    """
+    expression     → assignment ;
+    """
     return self.__assignment()
 
   def __assignment(self) -> Expr:
+    """
+    assignment     → ( call "." )? IDENTIFIER "=" assignment
+               | logic_or;
+    """
     expr = self.__logic_or()
     if self.__match(TokenType.EQUAL):
       equals: Token = self.__peek()
@@ -169,6 +235,9 @@ class Parser:
     return expr
 
   def __logic_or(self) -> Expr:
+    """
+    logic_or       → logic_and ( "or" logic_and )* ;
+    """
     left = self.__logic_and()
     while self.__match(TokenType.OR):
       operator = self.__peek()
@@ -179,6 +248,9 @@ class Parser:
 
 
   def __logic_and(self) -> Expr:
+    """
+    logic_and      → equality ( "and" equality )* ;
+    """
     left = self.__equality()
     while self.__match(TokenType.AND):
       operator = self.__peek()
@@ -188,6 +260,9 @@ class Parser:
     return left
 
   def __equality(self) -> Expr:
+    """
+    equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+    """
     expr = self.__comparison()
     while self.__match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL):
       operator = self.__peek()
@@ -197,6 +272,9 @@ class Parser:
     return expr
 
   def __comparison(self) -> Expr:
+    """
+    comparison     → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
+    """
     expr =  self.__addition()
     while self.__match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL):
       operator = self.__peek()
@@ -206,6 +284,9 @@ class Parser:
     return expr
 
   def __addition(self) -> Expr:
+    """
+    addition       → multiplication ( ( "-" | "+" ) multiplication )* ;
+    """
     expr =  self.__multiplication()
     while self.__match(TokenType.PLUS, TokenType.MINUS):
       operator = self.__peek()
@@ -215,6 +296,9 @@ class Parser:
     return expr
 
   def __multiplication(self) -> Expr:
+    """
+    multiplication → unary ( ( "/" | "*" ) unary )* ;
+    """
     expr =  self.__unary()
     while self.__match(TokenType.SLASH, TokenType.STAR):
       operator = self.__peek()
@@ -224,6 +308,9 @@ class Parser:
     return expr
 
   def __unary(self) -> Expr:
+    """
+    unary          → ( "!" | "-" ) unary | call ;
+    """
     if self.__match(TokenType.BANG, TokenType.MINUS):
       operator = self.__peek()
       self.__advance()
@@ -232,6 +319,9 @@ class Parser:
     return self.__call()
 
   def __call(self) -> Expr:
+    """
+    call           → primary ( "(" arguments? ")" | "." IDENTIFIER )* ;
+    """
     expr = self.__primary()
     while True:
       if self.__match_then_advance(TokenType.LEFT_PAREN):
@@ -256,6 +346,11 @@ class Parser:
 
 
   def __primary(self):
+    """
+    primary        → "true" | "false" | "nil" | "this"
+               | NUMBER | STRING | IDENTIFIER | "(" expression ")"
+               | "super" "." IDENTIFIER ;
+    """
     if self.__match_then_advance(TokenType.TRUE):
       return Literal(True)
     if self.__match_then_advance(TokenType.FALSE):
@@ -286,6 +381,8 @@ class Parser:
       return Variable(name=variable)
 
   def __synchronize(self):
+    """ synchronize parser to next statement to recover from panic(error) mode.
+    """
     while not self.__is_at_end():
       if self.__match_then_advance(TokenType.SEMICOLON):
         return
@@ -295,15 +392,21 @@ class Parser:
       self.__advance()
 
   def __match(self, *types) -> bool:
+    """check whether current token match expected Token types
+    """
     return any([self.__check(t) for t in types])
 
   def __match_then_advance(self, *types) -> bool:
+    """check whether current token match expected Token types. advance if match.
+    """
     if self.__match(*types):
       self.__advance()
       return True
     return False
 
   def __consume(self, expected: TokenType, err_msg: str):
+    """consume expected TokenType, raise a ParseError otherwise.
+    """
     if self.__match(expected):
       token = self.__peek()
       self.__advance()
@@ -313,18 +416,28 @@ class Parser:
       raise ParseError(err_msg)
 
   def __check(self, token_type: TokenType) -> bool:
+    """check exactly TokenType
+    """
     if self.__is_at_end(): return False
     return self.__peek().type == token_type
 
   def __is_at_end(self) -> bool:
+    """check whether current token is at the end of source code.
+    """
     return self.__peek().type == TokenType.EOF
 
   def __peek(self) -> Token:
+    """return current token
+    """
     return self.tokens[self.current]
 
   def __lookahead(self) -> Token:
+    """lookahead one token
+    """
     return self.tokens[self.current + 1]
 
   def __advance(self):
+    """move to next token
+    """
     if not self.__is_at_end():
       self.current += 1
